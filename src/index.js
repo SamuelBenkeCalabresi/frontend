dd2d19e6277f316da11a9318a80ee35c906b9a41
Samuel Benke Calabresi <samuelbenkecalabresi@Nazrins-MBP.home-life.hub>
2020-06-06 21:04:48 +0200

    starting react app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './components/App';
+import './index.css';
+
+render(
+    <App />,
+    document.getElementById('app')
+);
